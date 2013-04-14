4438bd6e9aadc7b2d9dacbb6bb40789cee6471d7
Eric Hardy <Eric.Hardy@univ-brest.fr>
2013-04-15 00:03:13 +0200

    cleanup examples

--- collective/js/abcjs/examples/example2.js
+++
@@ -1,5 +0,0 @@
-/* must create a new js in custom and register it,
- *  which contains this for use with example2.pt */
-window.onload = function() {
-	abc_editor = new ABCJS.Editor("abc", { canvas_id: "canvas0", midi_id:"midi", warnings_id:"warnings" });
-}
